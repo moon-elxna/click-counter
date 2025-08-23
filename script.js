34185736d72cf6f225bf66bb26eda29d4694b0cc
mooon-elxna <kreinelina@gmail.com>
2025-08-23 16:41:04 +0200

    implemented js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function decreaseNumber(){
-
+    document.getElementById("counter").innerHTML = Number(document.getElementById("counter").innerHTML) - 1;
 }
 
 function resetNumber(){
-
+    document.getElementById("counter").innerHTML = 0;
 }
 
 function increaseNumber(){
-    
+    document.getElementById("counter").innerHTML = Number(document.getElementById("counter").innerHTML) + 1;
 }
